da7b00fe4f1ec97d900a7b62a20de6a8c9056f3f
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-07-19 00:09:18 -0700

    added projects-router dependencies

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,9 @@
+const express = require('express');
+
+const Projects = require('../projects/projects-model');
+const router = express.router();
+
+
+
+module.exports = router;
 // Write your "projects" router here!
