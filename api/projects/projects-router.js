bc76303bd4fe72686fb638922647c78664a91bc1
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-07-19 01:15:24 -0700

    new project can be posted, returns 400 on missing params

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -21,5 +21,33 @@ router.get('/',
       }));
   });
 
+router.get('/:id',
+  async (req, res) => {
+    try {
+      const project = await Projects.get(req.params.id);
+      if (!project) {
+        res.status(404).json({ message: 'unable to find project' });
+      } else {
+        res.json(project);
+      }
+    } catch (error) {
+      res.status(500).send('unable to complete request').json({ stack: error.stack });
+    }
+  });
+
+router.post('/',
+  async (req, res) => {
+    try {
+      const {description, name} = req.body;
+      if (!name || !description) {
+        res.status(400).json({ message: 'unable to add project, please check that you have provided a name, and description' });
+      } else {
+       const newProject =  await Projects.insert({name, description});
+       res.json(newProject)
+      }
+    } catch (error) {
+      res.status(500).send('unable to complete request').json({ stack: error.stack });
+    }
+  })
 module.exports = router;
 // Write your "projects" router here!
